7900ac4e55aa49bd4a270a082731ee3e279f392e
pimpae <pimpae2014@gmail.com>
2025-09-23 13:09:11 +0700

    Update Leave.js

--- src/screens/Leave.js
+++ src/screens/Leave.js
@@ -1,20 +1,45 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Button, Text } from 'react-native-paper';
+import { View, Image } from 'react-native';
+import { Text, Appbar, TouchableRipple , Card } from 'react-native-paper';
+import styles from '../styles/style'; 
+import { MaterialCommunityIcons } from '@expo/vector-icons';  
 
 export default function Leave({ navigation }) {
+ 
+
   return (
-    <View style={styles.container}>
-      <Text variant="headlineMedium">Leave</Text>
-      <Button mode="contained" onPress={() => navigation.navigate('CheckIn')} style={styles.button}>Check In</Button>
-      <Button mode="contained" onPress={() => navigation.navigate('History')} style={styles.button}>History</Button>
-      <Button mode="contained" onPress={() => navigation.navigate('Schedule')} style={styles.button}>Schedule</Button>
-      <Button mode="contained" onPress={() => navigation.navigate('Leave')} style={styles.button}>Leave</Button>
+    <View style={{ flex: 1 }}> 
+      <Appbar.Header style={styles.appbar}>
+        <Appbar.Action
+          icon="menu" 
+          color="#ff3b30"
+          onPress={() => navigation.openDrawer()}
+        />
+        <Appbar.Content title="การลา" titleStyle={{ textAlign: 'center', color: "white" }} /> 
+        <Appbar.Action
+          icon="bell"
+          color="#ff3b30"
+          onPress={() => console.log("กดแจ้งเตือน")}
+        />
+      </Appbar.Header>
+
+ 
+      <Card style={{ margin: 16, borderRadius: 16, padding: 16 }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Image 
+            source={{ uri: 'https://i.pravatar.cc/150?img=12' }}
+            style={{ width: 60, height: 60, borderRadius: 30, marginRight: 16 }}
+          />
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: 'bold' }}>สมชาย ใจดี</Text>
+            <Text style={{ fontSize: 14, color: 'gray' }}>รหัสพนักงาน: SES00001</Text>
+            <Text style={{ fontSize: 14, color: 'gray' }}>ตำแหน่ง: พนักงานเซอร์เวย์</Text>
+          </View>
+        </View>
+      </Card>
+
+
+ 
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center', padding: 16 },
-  button: { marginVertical: 8, width: '80%' }
-});
